8171c36c9b9b8c782d2e0b24dd2a9c8cfc9343c6
anurag-adm <anurag026mishra@gmail.com>
2023-05-10 23:42:51 +0530

    Added Remove duplicate elements from sorted Array

---
+++ remove_dup_array.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int remove_duplicate(int a[], int n)
+{
+    // code here
+    int index = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[index] != a[i])
+        {
+            a[++index] = a[i];
+        }
+    }
+    return index + 1;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    int a[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];
+    }
+
+    int n = remove_duplicate(a, N);
+
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    return 0;
+}
