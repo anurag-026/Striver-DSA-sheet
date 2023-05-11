a530f10705e6b54c8dc8845acdd61cf8e83f4989
anurag-adm <anurag026mishra@gmail.com>
2023-05-11 19:41:33 +0530

    Modified

--- Quick_Left_Rotation.cpp
+++ Quick_Left_Rotation.cpp
@@ -7,37 +7,36 @@ void leftRotateM1(int a[], int k, int n)
     
     k = k % n;
     int temp[k];
-    for (int i = 0; i < k; i++)
+    for (int i = 0; i < k; i++)  // O(k)
     {
         temp[i] = a[i];
     }
 
-    for (int i = k; i < n; i++)
+    for (int i = k; i < n; i++) //O(n-k)
     {
         a[i - k] = a[i];
     }
     
-    for (int i = n-k; i < n; i++)
+    for (int i = n-k; i < n; i++) // O(k)
     {
         a[i] = temp[i - (n-k)];
         
     }
+    // Time Complexity = O(k) + O(n-k) + O(k) ==>  O(n+k)
+    // Space Complexity (extra)= O(k)    using array temp of k size
+
+
 }
 
-void revArray( int a[], int start, int end ){
-    
-}
 
+void leftRotateM2(int a[], int k, int n){
+    k=k%n;
+    reverse(a,a+k);
+    reverse(a+k, a+n);
+    reverse(a,a+n);
 
-void leftRotateM1(int a[], int k, int n){
-
-    revArray(a, 0, k);
-    revArray(a, k+1, n);
-
-
-
-
-
+    // Time Complexity = O(k) + O(n-k) + O(n) ==>  O(2n)
+    // Space Complexity (extra)= O(1)   Not using any extra space
 
 }
 
@@ -55,7 +54,7 @@ int main()
         cin >> a[i];
     }
 
-    leftRotateM1(a, k, n);
+    leftRotateM2(a, k, n);
 
     for (int i = 0; i < n; i++)
         cout << a[i] << " ";
