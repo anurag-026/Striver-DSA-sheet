b839cae17433fee331ebb71d2ed3ad9580698b90
anurag-adm <anurag026mishra@gmail.com>
2023-05-13 23:59:25 +0530

    13May

---
+++ Array/Easy/twoSumProbem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Count pairs with given sum
+int Pair_Sum(int arr[], int size, int sum){
+   int count = 0;
+   for (int i=0; i<size; i++){
+      for (int j=i+1; j<size; j++){
+         int total = arr[i] + arr[j];
+         if (total == sum){
+            count++;
+         }
+      }
+   }
+   return count;
+}
+int main(){
+   int arr[] = {2, 6, 1, 7, 9, 8} ;
+   int sum = 9;
+   int size = sizeof(arr)/sizeof(arr[0]);
+   cout<<"Count of pairs with given sum "<<sum<<" is: "<<Pair_Sum(arr, size, sum);
+   return 0;
+}
