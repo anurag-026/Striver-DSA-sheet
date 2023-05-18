af7bcd1f4b30ca93f45a67e30351d11e27fd52e1
anurag-adm <anurag026mishra@gmail.com>
2023-05-18 21:39:56 +0530

    18May

--- Array/Medium/Stock_buy_sell.cpp
+++ Array/Medium/Stock_buy_sell.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int maxProfit(int price[], int start, int end)
 {
 
@@ -10,17 +9,13 @@ int maxProfit(int price[], int start, int end)
 
 	int profit = 0;
 
-	for (int i = start; i < end; i++) {
-
-		for (int j = i + 1; j <= end; j++) {
-
-			if (price[j] > price[i]) {
-
-				int curr_profit
-					= price[j] - price[i]
-					+ maxProfit(price, start, i - 1)
-					+ maxProfit(price, j + 1, end);
-
+	for (int i = start; i < end; i++)
+	{
+		for (int j = i + 1; j <= end; j++)
+		{
+			if (price[j] > price[i])
+			{
+				int curr_profit = price[j] - price[i] + maxProfit(price, start, i - 1) + maxProfit(price, j + 1, end);
 				profit = max(profit, curr_profit);
 			}
 		}
@@ -28,24 +23,21 @@ int maxProfit(int price[], int start, int end)
 	return profit;
 }
 
-
-
-
 void stockBuySell(int price[], int n)
 {
-    maxProfit(price[], 0, n-1);
+	maxProfit(price[], 0, n - 1);
 }
 
 int main()
 {
 
-    int n, i;
-    cin >> n;
-    int price[n];
-    for (i = 0; i < n; i++)
-        cin >> price[i];
+	int n, i;
+	cin >> n;
+	int price[n];
+	for (i = 0; i < n; i++)
+		cin >> price[i];
 
-    stockBuySell(price, n);
+	stockBuySell(price, n);
 
-    return 0;
+	return 0;
 }
