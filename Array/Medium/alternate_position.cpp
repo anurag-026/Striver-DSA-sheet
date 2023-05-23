fb455d2a0b2152289ff9e97bf821ae389434794d
anurag-adm <anurag026mishra@gmail.com>
2023-05-23 19:19:00 +0530

    23 May

--- Array/Medium/alternate_position.cpp
+++ Array/Medium/alternate_position.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main()
 {
 
-    
+      int n;
+    cin >> n; // input size of array
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i]; // inputting elements of array
+
+    cout << maxSubarraySum(a, n) << endl;
+
     return 0;
 }
