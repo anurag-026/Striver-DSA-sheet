f9deb6cf1d393038ad6071bfefdf728406dde872
anurag-adm <anurag026mishra@gmail.com>
2023-05-26 18:27:53 +0530

    26 May

--- Array/Medium/next_permutation.cpp
+++ Array/Medium/next_permutation.cpp
@@ -2,27 +2,25 @@
 using namespace std;
 
 vector<int> nextGreaterPermutation(vector<int> &A) {
-    int n = A.size(); // size of the array.
+    int n = A.size(); 
 
-    // Step 1: Find the break point:
-    int ind = -1; // break point
+  
+    int ind = -1;
     for (int i = n - 2; i >= 0; i--) {
         if (A[i] < A[i + 1]) {
-            // index i is the break point
+          
             ind = i;
             break;
         }
     }
 
-    // If break point does not exist:
+
     if (ind == -1) {
-        // reverse the whole array:
+       
         reverse(A.begin(), A.end());
         return A;
     }
 
-    // Step 2: Find the next greater element
-    //         and swap it with arr[ind]:
 
     for (int i = n - 1; i > ind; i--) {
         if (A[i] > A[ind]) {
@@ -31,7 +29,7 @@ vector<int> nextGreaterPermutation(vector<int> &A) {
         }
     }
 
-    // Step 3: reverse the right half:
+   
     reverse(A.begin() + ind + 1, A.end());
 
     return A;
