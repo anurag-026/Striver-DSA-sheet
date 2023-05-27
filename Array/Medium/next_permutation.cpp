e10edfdc2d0681a419b9cf19e76c179f93837308
anurag-adm <anurag026mishra@gmail.com>
2023-05-27 22:01:44 +0530

    27 may

--- Array/Medium/next_permutation.cpp
+++ Array/Medium/next_permutation.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// function
 vector<int> nextGreaterPermutation(vector<int> &A) {
     int n = A.size(); 
 
@@ -13,37 +13,37 @@ vector<int> nextGreaterPermutation(vector<int> &A) {
             break;
         }
     }
-
-
     if (ind == -1) {
        
         reverse(A.begin(), A.end());
         return A;
     }
-
-
     for (int i = n - 1; i > ind; i--) {
         if (A[i] > A[ind]) {
             swap(A[i], A[ind]);
             break;
         }
     }
-
-   
     reverse(A.begin() + ind + 1, A.end());
 
     return A;
 }
+
 
 int main()
 {
     vector<int> A = {2, 1, 5, 4, 3, 0, 0};
     vector<int> ans = nextGreaterPermutation(A);
 
+
     cout << "The next permutation is: [";
     for (auto it : ans) {
         cout << it << " ";
     }
+
+
     cout << "]\n";
+
+
     return 0;
 }
