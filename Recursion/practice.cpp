25d9b5de1b197346eb83704ffbe47e581bb4466b
anurag-adm <anurag026mishra@gmail.com>
2023-05-31 16:53:55 +0530

    31 May

--- Recursion/practice.cpp
+++ Recursion/practice.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 // print from 1 to n (Backtracking)
-void print_1_to_n_BT(int n)
+void print_1_to_n_bt(int n)
 {
    if (n == 0)
    {
       return;
    }
-   print_1_to_n_BT(n - 1);
+   print_1_to_n_bt(n - 1);
    cout << n << " ";
    return;
 }
@@ -19,7 +19,7 @@ void print_1_to_n(int n)
    static int i = 0;
    i++;
    cout << i << " ";
-   if (i == n)
+   if (i == n) 
       return;
    print_1_to_n(n);
    return;
@@ -30,13 +30,23 @@ void print_n_to_1(int n){
    if(n==0) return;
    cout<<n<<" ";
    print_n_to_1(n-1);
-   return;
+   return; 
+}
+
+// print from n to 1 Backtracking
+void print_n_to_1_bt(int n){
+   static int i=0;
+   if(n==0) return;
+   print_n_to_1_bt(n-1);
+   i++;
+   cout<<i<<" ";
    
 }
 
+
 int main()
 {
-   print_n_to_1(5);
+   print_n_to_1_bt(5);
 
    return 0;
 }
