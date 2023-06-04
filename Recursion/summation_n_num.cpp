907bc46133cb216fbc6b30c376382b7c69c56041
anurag-adm <anurag026mishra@gmail.com>
2023-06-04 22:52:25 +0530

    4 June

--- Recursion/summation_n_num.cpp
+++ Recursion/summation_n_num.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 // Summation of 1st n numbers
-
+int summation(int n){
+   static int sum=0;
+   if(n==0)  return sum;
+   sum +=n;
+   return summation(n-1);
+}
 
 
 int main()
 {
+   cout<<summation(5);
 
    return 0;
 }
