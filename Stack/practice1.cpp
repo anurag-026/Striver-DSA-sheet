d3201e827f63759de7de83ad9013aac1c2c7e59d
anurag-adm <anurag026mishra@gmail.com>
2023-06-13 23:37:47 +0530

    13 JUNE

--- Stack/practice1.cpp
+++ Stack/practice1.cpp
@@ -1,3 +1,4 @@
+/*
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -37,6 +38,48 @@ int main() {
   vector<int> ans = printLeadersBruteForce(arr,n);
   
   for(int i = 0;i<ans.size();i++){
+      
+      cout<<ans[i]<<" ";
+  }
+  
+  cout<<endl;
+  return 0;
+}
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> printLeaders(int arr[], int n) {
+
+  vector<int> ans;
+
+ int max = arr[n - 1];
+ ans.push_back(arr[n-1]);
+
+
+  for (int i = n - 2; i >= 0; i--)
+    if (arr[i] > max) {
+      ans.push_back(arr[i]);
+      max = arr[i];
+    }
+
+  
+  return ans;
+}
+
+int main() {
+    
+
+  int n = 6;
+  int arr[n] = {10, 22, 12, 3, 0, 6};
+
+  vector<int> ans = printLeaders(arr,n);
+  
+  
+  for(int i = ans.size()-1;i>=0;i--){
       
       cout<<ans[i]<<" ";
   }
